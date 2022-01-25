a3f40364ffaa751f9dc50c64b5224b1fafd63fab
EriolEandur <Eriol_Eandur@gmx.de>
2022-01-25 22:29:14 +0100

    fixed dependencies

--- src/main/java/com/mcmiddleearth/minigames/game/AbstractGame.java
+++ src/main/java/com/mcmiddleearth/minigames/game/AbstractGame.java
@@ -7,18 +7,14 @@ package com.mcmiddleearth.minigames.game;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
-import com.mcmiddleearth.guidebook.command.GuidebookOff;
 import com.mcmiddleearth.minigames.MiniGamesPlugin;
 import com.mcmiddleearth.minigames.data.PluginData;
 import com.mcmiddleearth.minigames.scoreboard.GameScoreboard;
 import com.mcmiddleearth.minigames.utils.GameChatUtil;
-import com.mcmiddleearth.minigames.game.GeoGuessrGame;
-import com.mcmiddleearth.plugins.dynamicbooks.DynamicBooksPlugin;
 import com.mcmiddleearth.pluginutil.PlayerUtil;
 import com.mcmiddleearth.pluginutil.message.FancyMessage;
 import com.mcmiddleearth.pluginutil.message.MessageType;
 import org.bukkit.*;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.*;
@@ -27,10 +23,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Team;
 import org.bukkit.util.Vector;
-import com.mcmiddleearth.plugins.dynamicbooks.manager.BookManager;
-import com.mcmiddleearth.guidebook.command.GuidebookOff;
 
-import java.awt.print.Book;
 import java.util.*;
 import java.util.logging.Logger;
 
