cb661fd4facb6a78f052db22f0c693732e58cc6f
Jubo30 <julian.bornemann@gmx.net>
2022-09-18 16:30:09 +0200

    GeoGuessr Blacklist Fixes

--- src/main/java/com/mcmiddleearth/minigames/game/GeoGuessrGame.java
+++ src/main/java/com/mcmiddleearth/minigames/game/GeoGuessrGame.java
@@ -468,7 +468,7 @@ public class GeoGuessrGame extends AbstractGame implements Listener {
         PluginData.getMessageUtil().sendInfoMessage(player,"Don´t forget to set the number of rounds [/game setrounds x] . Default is 5.");
         PluginData.getMessageUtil().sendInfoMessage(player,"Don´t forget to set the area of warps [/game setarea x] . Default is a = all.");
         PluginData.getMessageUtil().sendInfoMessage(player,"Switch signtext on with /game allow signs.");
-        PluginData.getMessageUtil().sendInfoMessage(player,"Switch equal points on with /game allow points.");
+        PluginData.getMessageUtil().sendInfoMessage(player,"Switch equal points on with /game deny points.");
         PluginData.getMessageUtil().sendInfoMessage(player,"Do /game ready when you are done or have nothing done.");
     }
 
