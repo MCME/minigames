e0c5368b5c9ffc59082a1623119d222cf336237f
Jubo30 <julian.bornemann@gmx.net>
2022-12-01 23:13:29 +0100

    Start for Solo/smaller Gamemodes as Jump and Run or Toin Coss(?) not sure yet

---
+++ src/main/java/com/mcmiddleearth/minigames/MicroGame/AbstractMicroGame.java
@@ -0,0 +1,12 @@
+package com.mcmiddleearth.minigames.MicroGame;
+
+import com.mcmiddleearth.minigames.game.GameType;
+import org.bukkit.entity.Player;
+
+public abstract class AbstractMicroGame {
+
+
+    public AbstractMicroGame(Player manager, GameType type){
+
+    }
+}
---
+++ src/main/java/com/mcmiddleearth/minigames/command/GameJump.java
@@ -0,0 +1,29 @@
+package com.mcmiddleearth.minigames.command;
+
+import com.mcmiddleearth.minigames.MicroGame.AbstractMicroGame;
+import com.mcmiddleearth.minigames.data.PluginData;
+import com.mcmiddleearth.minigames.game.AbstractGame;
+import com.mcmiddleearth.minigames.MicroGame.JumpGame;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Jubo
+ */
+public class GameJump extends AbstractGameCommand{
+
+    public GameJump(String... permissionNodes){
+        super(0,true,permissionNodes);
+        cmdGroup = CmdGroup.JUMP;
+        setShortDescription("");
+        setUsageDescription("");
+    }
+
+    @Override
+    protected void execute(CommandSender cs,String... args){
+        PluginData.stopSpectating((Player)cs);
+        AbstractMicroGame game = new JumpGame((Player)cs);
+        PluginData.setGameChat((Player)cs,true);
+    }
+}
